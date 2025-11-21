d31cbc05c807b490807a50c1f280c0a3795071cb
haha1488top-source <haha1488top@gmail.com>
2025-11-21 13:50:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,22 @@
-const CACHE_NAME = 'rico-pwa-v5';
+const CACHE_NAME = 'rico-pwa-v6';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './img/rico_egg_rico_pin.png',
-    './img/icon.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './img/rico_egg_rico_pin.png',
+  './img/icon.png'
 ];
 
 self.addEventListener('install', (event) => {
-    console.log('Service Worker встановлюється');
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Кешуються файли');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Повертаємо кешовану версію або робимо запит
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                // Fallback для головної сторінки
-                if (event.request.mode === 'navigate') {
-                    return caches.match('./index.html');
-                }
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
